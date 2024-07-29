3115134b44456250d91bc70cc23a93cd52647080
MoMiAnn <vonguyenthanhtin.it@gmail.com>
2024-07-29 13:02:16 +0700

    Update Bai_1.cpp

--- Bai_1.cpp
+++ Bai_1.cpp
@@ -1,10 +1,10 @@
-//Bài 1: Tính S(n) = 1 + 2 + 3 + … + n
+//BÃ i 1: TÃ­nh S(n) = 1 + 2 + 3 + â€¦ + n
 #include <iostream>
 using namespace std;
 
 int tong_S(int n){
 	int tong = 0;
-	for(int i=1; i < n; i++){
+	for(int i=1; i <= n; i++){
 		tong+=i; 
 	}
 	return tong; 
